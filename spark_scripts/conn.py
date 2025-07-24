f7ab63dab8b6406247d6e3b9b6596d9e690f9adc
Ansh <anshnain10@gmail.com>
2025-07-24 22:59:44 +0200

    Moved login credentials to .env file and updated conn.py

--- spark_scripts/conn.py
+++ spark_scripts/conn.py
@@ -1,19 +1,16 @@
+from dotenv import load_dotenv
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-csv_path = "/Users/ansh/etl-pipeline/output/cleaned_data/cleaned_data.csv"
+load_dotenv()
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+db = os.getenv("POSTGRES_DB")
+
+csv_path = "<path_to_your_cleaned_data.csv>"
 df = pd.read_csv(csv_path)
 
-# ✅ Use credentials from docker-compose.yml
-user = "retail_user"
-password = "retail_pass"     # <- use the actual password you set
-host = "localhost"
-port = "5432"
-database = "retail_db"
-
-# SQLAlchemy connection string
-url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
-engine = create_engine(url)
-
+engine = create_engine(f'postgresql://{user}:{password}@localhost:5432/{db}')
 df.to_sql("retail_data_cleaned", engine, if_exists="replace", index=False)
-print("✅ Data inserted into PostgreSQL successfully!")
